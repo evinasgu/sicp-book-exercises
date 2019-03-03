c4ded6356cdecea05e969ee6fce95e74728d8217
evinasgu <enyert.watson@gmail.com>
2019-03-03 01:55:46 -0500

    Applying local names feature and externalizing the tolerance value

--- sqrt-module.scm
+++ sqrt-module.scm
@@ -1,17 +1,14 @@
+(define tolerance-value (/ 1 100000000000000000))
+
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (cube-root-function guess x)
-  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
-
-(define (square-root-function guess x)
-  (/ x guess))
-
 (define (improve guess x f)
-  (average guess (f guess x)))
+  (average guess (f guess)))
 
 (define (good-enough? guess x f)
-  (< (abs (- guess (improve guess x f))) (* (/ 1 100000000000000000) guess)))                                                                                                                                    
+  (< (abs (- guess (improve guess x f)))
+     (* tolerance-value guess)))
 
 (define (general-iter guess x f)
   (if (good-enough? guess x f)
@@ -21,9 +18,13 @@
 		    f)))
 
 (define (my-sqrt x)
+  (define (square-root-function guess)
+    (/ x guess))
   (general-iter 1.0 x square-root-function))
 
 (define (my-cuberoot x)
+  (define (cube-root-function guess)
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
   (general-iter 1.0 x cube-root-function))
 
 ;(display (my-sqrt 10))
